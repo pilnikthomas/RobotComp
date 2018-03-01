22b5ec118105f6d49051007807ced429e7088e2b
Thomas Pilnik <thomaspo@al.insper.edu.br>
2018-03-01 12:06:27 -0300

    Atividade aula2

---
+++ Aula2/videoplay_cor.py
@@ -0,0 +1,113 @@
+# -*- coding:utf-8 -*-
+
+# Dupla: Maria Eduarda e Thomas Pilnik
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+
+# Para usar o vídeo
+#cap = cv2.VideoCapture('hall_box_battery_mp2.mp4')
+# As 3 próximas linhas são para usar a webcam
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+kernel = np.ones((5,5),np.uint8)
+
+
+def identifica_cor(frame):
+    '''
+    Segmenta o maior objeto cuja cor é parecida com cor_h (HUE da cor, no espaço HSV).
+    '''
+
+    # No OpenCV, o canal H vai de 0 até 179, logo cores similares ao 
+    # vermelho puro (H=0) estão entre H=-8 e H=8. 
+    # Veja se este intervalo de cores está bom
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cor_menor = np.array([0, 50, 50])
+    cor_maior = np.array([8, 255, 255])
+    segmentado_cor = cv2.inRange(frame_hsv, cor_menor, cor_maior)
+    cv2.imshow('inrange', segmentado_cor)
+    segmentado_cor = cv2.morphologyEx(segmentado_cor, cv2.MORPH_OPEN, kernel)
+
+    # Será possível limpar a imagem segmentado_cor? 
+    # Pesquise: https://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
+
+
+    # Encontramos os contornos na máscara e selecionamos o de maior área
+    img_out, contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+    maior_contorno = None
+    maior_contorno_area = 0
+
+
+    cv2.drawContours(frame, contornos, -1, [255, 0, 255], 5)
+
+
+    for cnt in contornos:
+        area = cv2.contourArea(cnt)
+
+
+        if area > maior_contorno_area:
+            maior_contorno = cnt
+            maior_contorno_area = area
+
+    
+    # Encontramos o centro do contorno fazendo a média de todos seus pontos.
+    if not maior_contorno is None:
+        cv2.drawContours(frame, [maior_contorno], -1, [0, 0, 255], 5)
+        maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
+
+        ymin= 500
+        ymax= 0
+       
+        for i in maior_contorno:
+        	#print(i[1])
+        	if i[1]>ymax:
+        		ymax=i[1]
+        	if i[1]<ymin:
+        		ymin= i[1]
+
+        h=ymax-ymin
+        media = maior_contorno.mean(axis=0)
+        media = media.astype(np.int32)
+        cv2.circle(frame, tuple(media), 5, [0, 255, 0])
+        dist=(1370.57*37)/h
+        print(dist)
+    else:
+        media = (0, 0)
+
+    cv2.imshow('', frame)
+    cv2.imshow('imagem in_range', segmentado_cor)
+    cv2.waitKey(1)
+
+    centro = (frame.shape[0]//2, frame.shape[1]//2)
+
+    return media, centro
+
+
+
+
+while(True):
+    # Capture frame-by-frame
+    print("Novo frame")
+    ret, frame = cap.read()
+
+
+    img = frame.copy()
+
+    media, centro = identifica_cor(img)
+
+    #More drawing functions @ http://docs.opencv.org/2.4/modules/core/doc/drawing_functions.html
+
+    # Display the resulting frame
+    cv2.imshow('original',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    print("No circles were found")
+    
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
